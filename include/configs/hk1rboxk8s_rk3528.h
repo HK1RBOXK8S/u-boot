7386d433bb793f7330783c4c8cebb9ddd924b198
lbrdev2020 <lbr-dev@yandex.com>
2026-08-26 21:47:21 +0800

    fix: fixed.

--- include/configs/hk1rboxk8s_rk3528.h
+++ include/configs/hk1rboxk8s_rk3528.h
@@ -8,12 +8,16 @@
 
 #define ROCKCHIP_DEVICE_SETTINGS \
 			"stdout=serial,vidconsole\0" \
-			"stderr=serial,vidconsole\0"
+			"stderr=serial,vidconsole\0" \
+			HK1RBOXK8S_RK3528_SETTINGS
 
 #include <configs/rk3528_common.h>
 
-#define CONFIG_BOOTCOMMAND \
-   ";" \
-   ""
+#define HK1RBOXK8S_RK3528_SETTINGS \
+    "bootargs=console=ttyS0,1500000 earlycon=uart8250,mmio32,0xff9f0000 loglevel=7 ignore_loglevel\0" \
+    "ipaddr=192.168.1.22\0" \
+    "netmask=255.255.255.0\0" \
+    "gatewayip=192.168.1.1\0" \
+    "serverip=192.168.1.1\0"
 
 #endif
